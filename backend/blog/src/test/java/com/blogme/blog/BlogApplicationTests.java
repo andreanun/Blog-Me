5f310294418cb4e33e07d7290c2b360419ea25bc
Andrea Nunoo <123588571+andreanun@users.noreply.github.com>
2025-01-25 21:06:21 -0500

    Initial commit: added bare frontend

---
+++ backend/blog/src/test/java/com/blogme/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogme.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
